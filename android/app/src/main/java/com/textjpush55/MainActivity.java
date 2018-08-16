a2363361bbc0acae50c4d04c3b443ed64f9e1023
jasder <jasduaner@gmail.com>
2018-08-16 16:26:46 +0800

    init project

--- android/app/src/main/java/com/textjpush55/MainActivity.java
+++ android/app/src/main/java/com/textjpush55/MainActivity.java
@@ -67,9 +67,9 @@ public class MainActivity extends ReactActivity {
 					 Toast.makeText(this, "通知栏打开", Build.VERSION_CODES.BASE).show(); 
 				} else {
 					Toast.makeText(this, "通知栏未打开", Build.VERSION_CODES.BASE).show(); 
-					
 
 	        // 检测到手机通知权限未开启，弹出提示框，让用户手机设置
+	        //  进入应用的权限设置界面
 	        builder = new AlertDialog.Builder(this);
 	        alertDialog = builder.setIcon(R.mipmap.ic_launcher)
 	          .setTitle("系统提示:")
@@ -103,58 +103,6 @@ public class MainActivity extends ReactActivity {
 	        alertDialog.show();
 
 				}
-
-
-
-				        // AlertDialog.Builder builder=new AlertDialog.Builder(this);
-				        // builder.setIcon(R.drawable.logo_mini);
-				        // builder.setTitle("下线通知").setMessage("该账号在另一台Android设备上登录。")
-				        // .setPositiveButton("重新登陆", new DialogInterface.OnClickListener() {
-				 
-				        //     @Override
-				        //     public void onClick(DialogInterface dialog, int which) {
-				        //         //do somthing
-				        //     }
-				        // }).setNegativeButton("退出",new DialogInterface.OnClickListener() {
-				 
-				        //     @Override
-				        //     public void onClick(DialogInterface dialog, int which) {
-				        //         Intent i=new Intent(CoreService.this,LoginActivity.class);
-				        //         i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-				        //         startActivity(i);
-				        //     }
-				        // });
-				        // alertDialog = builder.create();
-				        // alertDialog.setCancelable(false);
-				        // alertDialog.setCanceledOnTouchOutside(false);
-				        // alertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
-				        // alertDialog.show();
-
-				/*
-					供应商想给你发送推送通知
-
-					’通知’主要包括促销活动、下单、接单信息提醒，请在‘设置’中打开。	
-				*/
-
-				// builder = new AlertDialog.Builder(this);
-    //     alertDialog = builder.setIcon(R.mipmap.ic_launcher)
-    //       .setTitle("系统提示:")
-    //       .setMessage("‘通知’主要包括促销活动、下单、接单信息提醒，请在 ’设置‘ 中打开。")
-    //       .setNegativeButton("取消", new DialogInterface.OnClickListener() {
-    //           @Override
-    //           public void onClick(DialogInterface dialogInterface, int i) {
-
-    //           }
-    //       })
-    //       .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-    //           @Override
-    //           public void onClick(DialogInterface dialogInterface, int i) {
-
-    //           }
-    //       }).create();
-    //     alertDialog.show();
-				
-
     }
 
     @Override
